afb7a41ed0ac7b4084cd1383b4abbb22e772544b
Vishwas M S <vishwas.ms@updaptcsr.com>
2023-11-01 10:24:25 +0530

    setup

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import { StatusBar } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import {StatusBar} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import LoginScreen from './src/screens/LoginScreen';
-import { Colors } from './src/utils/Colors';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import {Colors} from './src/utils/Colors';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import RegisterScreen from './src/screens/RegisterScreen';
 import MainScreen from './src/screens/MainScreen';
 import auth from '@react-native-firebase/auth';
@@ -11,7 +11,6 @@ import auth from '@react-native-firebase/auth';
 const Stack = createStackNavigator();
 
 const App = () => {
-
   const [user, setUser] = useState();
 
   const onAuthStateChanged = (user: any) => setUser(user);
@@ -23,19 +22,19 @@ const App = () => {
 
   return (
     <NavigationContainer>
-      <StatusBar backgroundColor={Colors.white} barStyle='dark-content' />
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {user ?
+      <StatusBar backgroundColor={Colors.white} barStyle="dark-content" />
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        {true ? (
           <Stack.Screen name="MainScreen" component={MainScreen} />
-          :
+        ) : (
           <>
             <Stack.Screen name="LoginScreen" component={LoginScreen} />
             <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
           </>
-        }
+        )}
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default App;
